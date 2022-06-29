5bdaf17c21aca2547c48763bbfd47ad074e657db
Mehmet YILDIRIM <97946779+inmyildirim@users.noreply.github.com>
2022-06-29 20:05:13 +0300

    BasePage for Demo

--- src/test/java/com/demonblaze/pages/BasePage.java
+++ src/test/java/com/demonblaze/pages/BasePage.java
@@ -1,4 +1,19 @@
 package com.demonblaze.pages;
 
+import com.demonblaze.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+
 public class BasePage {
+
+    public BasePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    public void getLink(String link){
+        Driver.getDriver().findElement(By.partialLinkText(link)).click();
+    }
+
+
+
 }
